f3b3011dc86c020af8f4dd33c837f29122ec56da
Christian Vestre <christianvestre@ChristisMacBook.fritz.box>
2018-03-25 11:35:00 +0200

    Image switching working

--- ListsApp/PictureViewer.js
+++ ListsApp/PictureViewer.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import { Button, View, Image } from 'react-native';
 
+/*[require('./img/james_burke.jpeg'),
+              require('./img/james_burke.jpeg'),
+              require('./img/james_burke.jpeg')] */
 
-var images = ["{require('./img/james_burke.jpeg')}",
-              "{require('./img/james_burke.jpeg')}",
-              "{require('./img/james_burke.jpeg')}"]
 
 const state = 0
-console.log(images[0]);
+const images ={1:require('./img/james_burke.jpeg'),
+               2:require('./img/james_burke2.jpeg')}
 
 export default class DisplayAnImage extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {num:1};
+  }
+  renderImage() {
+    console.log(this.state.num)
+    return (
+       <Image
+         style={{width: 250, height: 250}}
+         source={images[this.state.num]}
+       />
+     );
+   }
+
   render() {
+    console.log(images)
     return (
       <View>
-        <Image
-          style={{width: 250, height: 250}}
-          source={require('./img/james_burke.jpeg')}
-        />
+        {this.renderImage()}
         <Button
-          onPress={() => {alert("it works");}}
+          onPress={() =>{this.setState({num: this.state.num + 1})}}
           title="Learn More"
           color="#841584"
           accessibilityLabel="Learn more about this purple button"
